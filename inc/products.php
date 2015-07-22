c1692ae409c67d9a5edc102a99a373884b262ff9
Kollyn Lund <kollyn.lund@gmail.com>
2015-07-23 02:03:15 +0300

    making patents.php dynamic

--- inc/products.php
+++ inc/products.php
@@ -5,11 +5,12 @@ function get_list_view_html($product_id, $product) {
     $output = "";
 
 
-    $output = $output . "<li>";
+    $output = $output . "<li " . "class='patent-list-details'" . '>';
     $output = $output . '<a href="shirt.php?id=' . $product_id . '">';
-    $output = $output . '<img src="' . $product["img"] . '" alt="' . $product["name"] . '">';
-    $output = $output . "<p>Patent 123</p>";
-    $output = $output . "<p>View Details</p>";
+//    $output = $output . '<img src="' . $product["img"] . '" alt="' . $product["name"] . '">';
+    $output = $output . "<p " . "class='patent-name'" . ">" . $product["name"] . "</p>";
+    $output = $output . "<span>" . " #: " . $product["patent-#"] . "</span>";
+    $output = $output . "<p>" . "* " . $product["description"] . "</p>";
     $output = $output . "</a>";
     $output = $output . "</li>";
 
@@ -19,61 +20,68 @@ function get_list_view_html($product_id, $product) {
 
 $products = array();
 $products[101] = array(
-	"name" => "Patent 123123",
+	"name" => "Isotruss",
+    "patent-#" => "7,423,332",
 	"img" => "img/shirts/shirt-101.jpg",
 	"price" => 11,
     "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
     and on the right. It has two wheels and moves fast.",
+	
     
 );
 $products[102] = array(
-	"name" => "Patent 5435435",
+	"name" => "CVT-1",
+    "patent-#" => "7,223,322",
     "img" => "img/shirts/shirt-102.jpg",
     "price" => 20,
     "paypal" => "SXKPTHN2EES3J",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 $products[103] = array(
-    "name" => "Patent 234323",
+    "name" => "CVT-2",
+    "patent-#" => "4,423,777",
     "img" => "img/shirts/shirt-103.jpg",    
     "price" => 20,
-    "paypal" => "7T8LK5WXT5Q9J",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 $products[104] = array(
-    "name" => "Patent 3242323",
+    "name" => "Search Software",
+    "patent-#" => "7,667,554",
     "img" => "img/shirts/shirt-104.jpg",    
     "price" => 18,
-    "paypal" => "YKVL5F87E8PCS",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 $products[105] = array(
     "name" => "Patent 2342344",
     "img" => "img/shirts/shirt-105.jpg",    
     "price" => 25,
-    "paypal" => "4CLP2SCVYM288",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
+
 );
 $products[106] = array(
     "name" => "Patent 777675654",
     "img" => "img/shirts/shirt-106.jpg",    
     "price" => 20,
-    "paypal" => "TNAZ2RGYYJ396",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 $products[107] = array(
     "name" => "Patent 554443433",
     "img" => "img/shirts/shirt-107.jpg",    
     "price" => 20,
-    "paypal" => "S5FMPJN6Y2C32",
-    "sizes" => array("Small","Medium","Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 $products[108] = array(
     "name" => "Patent 4433343",
     "img" => "img/shirts/shirt-108.jpg",    
     "price" => 25,
-    "paypal" => "JMFK7P7VEHS44",
-    "sizes" => array("Large","X-Large")
+    "description" => "This patent is the best in the world! Created by Kollyn Lund and Joe. It helps balance weight on the left side 
+    and on the right. It has two wheels and moves fast.",
 );
 
 ?>
--- patents.php
+++ patents.php
@@ -58,13 +58,15 @@ include('inc/header.php'); ?>
 
 
 
-<!-- 
 				<ul class="products">
 					<?php foreach($products as $product_id => $product) { 
 							echo get_list_view_html($product_id,$product);
 						}
 					?>
-				</ul> -->
+				</ul>
+
+
+
 
 			</div>
 
