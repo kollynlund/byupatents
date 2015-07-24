bd333f22d184a9ee31f0a74f474c6de12be4474a
StevenBillings <carter_smith_12@hotmail.com>
2015-07-24 11:57:26 +0300

    contact page works

--- contact.php
+++ contact.php
@@ -1,6 +1,11 @@
 <?php 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include_once('inc/phpmailer/class.phpmailer.php');
+
+    require_once('inc/phpmailer/class.smtp.php');
+
+
     $name = trim($_POST["name"]);
     $email = trim($_POST["email"]);
     $message = trim($_POST["message"]);
@@ -39,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $mail->Host = 'localhost';
     $mail->SetFrom($email, $name);
-    $address = "kollyn.lund@gmail.com";
+    $address = "carter.james.smith.12@gmail.com";
     $mail->AddAddress($address, "Shirts 4 Mike");
     $mail->Subject    = "Shirts 4 Mike Contact Form Submission | " . $name;
     $mail->MsgHTML($email_body);
@@ -64,7 +69,7 @@ include('inc/header.php'); ?>
             <h1>Contact</h1>
 
             <?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
-                <p>Thanks for the email! I&rsquo;ll be in touch shortly!</p>
+                <p>Thanks for the email! We&rsquo;ll be in touch shortly!</p>
             <?php } else { ?>
 
                 <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
