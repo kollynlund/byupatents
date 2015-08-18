6c337379daa57400b703f4aad140821f560eef07
StevenBillings <carter_smith_12@hotmail.com>
2015-08-18 22:01:17 +0200

    Fixed the search pagination

--- technologies.php
+++ technologies.php
@@ -80,11 +80,66 @@ elseif (isset($_GET["s"])) {
 				<?php if (!empty($products)) : ?>
 					<ul class="products">
 						<?php
+
+												    // Find out how many items are in the table
+							    $total = count($products);
+
+							    If($total > 5){
+
+							    // How many items to list per page
+							    $limit = 5;
+
+							    // How many pages will there be
+							    $pages = ceil($total / $limit);
+
+							    // What page are we currently on?
+							    $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+							        'options' => array(
+							            'default'   => 1,
+							            'min_range' => 1,
+							        ),
+							    )));
+
+							    // Calculate the offset for the query
+							    $offset = ($page - 1)  * $limit;
+
+							    // Some information to display to the user
+							    $start = $offset + 1;
+							    $end = min(($offset + $limit), $total);
+							    // Prepare the paged query
+							    							    // The "back" link
+							    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+							    // The "forward" link
+							    $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+							    // Display the paging information
+							    echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+
+							        // Display the results
+							    $keys = array_keys($products);
+							
+							
+							    for($i = $start; $i <= $end; $i++) {
+							    		$key = $keys[$i-1];
+										echo get_list_view_html($key,$products[$key]);
+							}
+														    // The "back" link
+							    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+							    // The "forward" link
+							    $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+							    // Display the paging information
+							    echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+							}
+							else{
 							foreach ($products as $product_id => $product) {
 	                          	$id_number = $product['id#'];
 	                          	$array_space = searchForID($id_number, get_products_all());
 	                          	echo get_list_view_html($array_space,$product);
-							}
+							}}
 						?>	
 					</ul>
 				<?php elseif($search==True): ?>
