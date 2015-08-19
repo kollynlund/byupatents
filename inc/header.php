b6e4d43aaa1ef715a322c081e79ef8ade9112595
Kollyn Lund <kollyn.lund@gmail.com>
2015-08-19 21:08:41 +0200

    fixed the hero image

--- inc/header.php
+++ inc/header.php
@@ -12,11 +12,6 @@
     <meta name="description" content="Welcome to BYU's Technology Transfer Office.">
 
 
-
-
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="js/scripts.js"></script>
-
 </head>
 <body>
 
